cf59301fde4575298023c48183a2b568d157d4aa
Seongha Jeon <gotppetto@gmail.com>
2020-01-05 01:13:28 +0900

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,12 +9,82 @@ const IndexPage = ( ) => {
     <Layout>
       <div className="layout__row">
         <div className="post__panel">
+          <h2>전성하</h2>
+          <h3>소개</h3>
           <ul>
             <li># 공유와 함께 성장하는 것을 좋아하고 좋은 개발문화에 관심이 많은 프론트엔드 개발자입니다.</li>
             <li># 디자인 시스템에 관심이 많고 CSS를 활용하여 UI를 제어하거나 마이크로 인터랙션을 구현하는 것을 좋아합니다.</li>
             <li># 동료와 함께 성장하고 그를 기반으로 시너지를 내어 생산성있는 팀워크를 만드는 목표가 있습니다.</li>
           </ul>
-          <p>양질의 글로 채우겠습니다. :) ... (작성 중!)</p>
+
+          <h3>보유 기술</h3>
+          <h4>Front-end</h4>
+          <dl>
+            <dt>img</dt>
+            <dd>HTML5</dd>
+          </dl>
+
+          <h4>Database</h4>
+          <dl>
+            <dt>img</dt>
+            <dd>sql</dd>
+          </dl>
+
+          <h4>Mobile</h4>
+          <dl>
+            <dt>img</dt>
+            <dd>swift</dd>
+          </dl>
+
+          <h3>경력</h3>
+          <dl>
+            <dt>패스트캠퍼스</dt>
+            <dd>2019.04.08~ 재직중</dd>
+            <dd>
+              <dl>
+                <dt>
+                  <em>사용 기술</em>
+                  - Node.js (express)
+                  - HTML5, CSS3, RWD
+                  - Vue, vuex, vue-router
+                </dt>
+                <dd>패스트캠퍼스 웹 사이트 개발 2019.4 - 2020.1</dd>
+                <dd><a href="https://github.com/fastcampusgit/markup-guide" target="_blank">프론트엔드 마크업 가이드 제작</a> </dd>
+              </dl>
+            </dd>
+          </dl>
+          <dl>
+            <dt>디지털 에이전시 이롭게</dt>
+            <dd>2013.04.01~2018.06.28</dd>
+            <dd>
+              <dl>
+                <dt>
+                  <em>사용 기술</em>
+                  - webpack, gulp, grunt
+                  - jQuery, gsap (인터랙티브 페이지 개발)
+                  - HTML5, CSS3, RWD, 웹 접근성
+                </dt>
+                <dd>헤라 브랜드 사이트 (2017)</dd>
+                <dd>CJ 문화재단 브랜드 사이트 (2017)</dd>
+                <dd>J.ESTINA BEAUTY 커머스 사이트 (2017)</dd>
+                <dd>닥터폰트 브랜드 사이트 (2016)</dd>
+                <dd>아모레퍼시픽 그룹 브랜드 사이트 (2016)</dd>
+                <dd>샘표 커머스 사이트 (2016)</dd>
+                <dd>오설록 커머스 사이트 (2016)</dd>
+                <dd>설화수 브랜드 브랜드 (2015)</dd>
+                <dd>Yap 브랜드 사이트 (2015)</dd>
+                <dd>미쟝센 브랜드 사이트 (2015)</dd>
+                <dd>프리메라 브랜드 사이트 (2014)</dd>
+                <dd>헤라 브랜드 사이트 (2014)</dd>
+                <dd>한율 브랜드 사이트 (2014)</dd>
+                <dd>암시민연대 브랜드 사이트 (2014)</dd>
+                <dd>아시안브릿지 브랜드 사이트 (2013)</dd>
+                <dd>에이콘 출판사 브랜드 사이트 (2013)</dd>
+                <dd>청담러닝 스마트북 (2013)</dd>
+                <dd>드림 성형 외과 브랜드 사이트 (2013)</dd>
+              </dl>
+            </dd>
+          </dl>
         </div>
       </div>
     </Layout>

787ff12d2639f7514ce2aeaac50d24befc8d6f3d
Seongha Jeon <gotppetto@gmail.com>
2020-01-17 17:01:42 +0900

    Update my stacks

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 
@@ -23,7 +22,6 @@ import nextjs from '../assets/images/stacks/nextjs.png';
 import reactNative from '../assets/images/stacks/react-native.jpg';
 import styledComponents from '../assets/images/stacks/styled-components.png';
 import vue from '../assets/images/stacks/vue.png';
-
 
 // database
 import mysql from '../assets/images/stacks/mysql.png';
@@ -52,7 +50,7 @@ const IndexPage = ( ) => {
     <Layout>
       <div className="layout__row">
         <div className="post__panel">
-          <h2>전성하</h2>
+          <h2>전성하 <i className="job">프론트엔드</i></h2>
           <h3>소개</h3>
           <ul>
             <li># 공유와 함께 성장하는 것을 좋아하고 좋은 개발문화에 관심이 많은 프론트엔드 개발자입니다.</li>
@@ -198,14 +196,14 @@ const IndexPage = ( ) => {
           </ul>
 
           <h3>경력</h3>
+
           <dl>
-            <dt>패스트캠퍼스</dt>
-            <dd>2019.04.08~ 재직중</dd>
+            <dt>패스트캠퍼스 <i className="period">(2019.04.08~ 재직중)</i></dt>
             <dd>
               <dl>
                 <dt>
                   <em>사용 기술</em>
-                  <ul className="stack-list stack-list--use-stacks">
+                  <ul className="stack-list stack-list--organization">
                     <li>
                       <div className="stack-list__item"><img src={html} alt="html5"/></div>
                       <strong>HTML5</strong>
@@ -253,7 +251,7 @@ const IndexPage = ( ) => {
                   </ul>
 
                   <em>환경</em>
-                  <ul className="stack-list stack-list--use-stacks">
+                  <ul className="stack-list stack-list--organization">
                     <li>
                       <div className="stack-list__item"><img src={docker} alt="docker"/></div>
                       <strong>docker</strong>
@@ -268,19 +266,18 @@ const IndexPage = ( ) => {
                     </li>
                   </ul>
                 </dt>
-                <dd>패스트캠퍼스 웹 사이트 개발 2019.4 - 2020.1</dd>
-                <dd><a href="https://github.com/fastcampusgit/markup-guide" target="_blank">프론트엔드 마크업 가이드 제작</a> </dd>
+                <dd>패스트캠퍼스 웹 사이트 개발 <i className="period">(2019.4 - 2020.1)</i></dd>
+                <dd>프론트엔드 마크업 가이드 제작 <a href="https://github.com/fastcampusgit/markup-guide" target="_blank" rel="noreferrer noopener">[링크]</a> </dd>
               </dl>
             </dd>
           </dl>
           <dl>
-            <dt>디지털 에이전시 이롭게</dt>
-            <dd>2013.04.01~2018.06.28</dd>
+            <dt>디지털 에이전시 이롭게 <i className="period">(2013.04.01~2018.06.28)</i></dt>
             <dd>
               <dl>
                 <dt>
                   <em>사용 기술</em>
-                  <ul className="stack-list stack-list--use-stacks">
+                  <ul className="stack-list stack-list--organization">
                     <li>
                       <div className="stack-list__item"><img src={html} alt="html5"/></div>
                       <strong>HTML5</strong>
@@ -323,24 +320,60 @@ const IndexPage = ( ) => {
                     </li>
                   </ul>
                 </dt>
-                <dd>헤라 브랜드 사이트 (2017)</dd>
-                <dd>CJ 문화재단 브랜드 사이트 (2017)</dd>
-                <dd>J.ESTINA BEAUTY 커머스 사이트 (2017)</dd>
-                <dd>닥터폰트 브랜드 사이트 (2016)</dd>
-                <dd>아모레퍼시픽 그룹 브랜드 사이트 (2016)</dd>
-                <dd>샘표 커머스 사이트 (2016)</dd>
-                <dd>오설록 커머스 사이트 (2016)</dd>
-                <dd>설화수 브랜드 브랜드 (2015)</dd>
-                <dd>Yap 브랜드 사이트 (2015)</dd>
-                <dd>미쟝센 브랜드 사이트 (2015)</dd>
-                <dd>프리메라 브랜드 사이트 (2014)</dd>
-                <dd>헤라 브랜드 사이트 (2014)</dd>
-                <dd>한율 브랜드 사이트 (2014)</dd>
-                <dd>암시민연대 브랜드 사이트 (2014)</dd>
-                <dd>아시안브릿지 브랜드 사이트 (2013)</dd>
-                <dd>에이콘 출판사 브랜드 사이트 (2013)</dd>
-                <dd>청담러닝 스마트북 (2013)</dd>
-                <dd>드림 성형 외과 브랜드 사이트 (2013)</dd>
+                <dd>
+                  헤라 브랜드 사이트 (2017) <a href="https://www.hera.com/kr/ko" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  CJ 문화재단 브랜드 사이트 (2017) <a href="https://www.cjazit.org/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  J.ESTINA BEAUTY 커머스 사이트 (2017)
+                </dd>
+                <dd>
+                  닥터폰트 브랜드 사이트 (2016) <a href="https://github.com/fastcampusgit/markup-guide" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  아모레퍼시픽 그룹 브랜드 사이트 (2016) <a href="https://www.apgroup.com/int/ko/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  샘표 커머스 사이트 (2016) <a href="https://www.sempio.com/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  오설록 커머스 사이트 (2016) <a href="https://www.osulloc.com/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  설화수 브랜드 브랜드 (2015) <a href="https://www.sulwhasoo.com/kr/ko/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  Yap 브랜드 사이트 (2015)
+                </dd>
+                <dd>
+                  미쟝센 브랜드 사이트 (2015) <a href="http://www.miseenscene.com/main.do" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  프리메라 브랜드 사이트 (2014) <a href="https://www.primera.co.kr/kr/ko/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  헤라 브랜드 사이트 (2014)
+                </dd>
+                <dd>
+                  한율 브랜드 사이트 (2014) <a href="https://www.hanyul.com/kr/ko/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  암시민연대 브랜드 사이트 (2014) <a href="https://www.apgroup.com/int/ko/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  아시안브릿지 브랜드 사이트 (2013) <a href="http://www.ilovecancer.org/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  에이콘 출판사 브랜드 사이트 (2013) <a href="http://acornpub.co.kr/" target="_blank" rel="noreferrer noopener">[링크]</a>
+                </dd>
+                <dd>
+                  청담러닝 스마트북 (2013)
+                </dd>
+                <dd>
+                  드림 성형 외과 브랜드 사이트 (2013)
+                </dd>
               </dl>
             </dd>
           </dl>

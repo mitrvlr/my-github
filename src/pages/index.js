a971ad78278aa763e9ea8025fdcb87f76c5b5f40
seongha <mitrvlr@gmail.com>
2021-09-24 12:34:26 +0900

    fixes fail build

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = async ({ data }) => {
+const IndexPage = ({ data }) => {
   const { edges: posts } = data.allMarkdownRemark;
 
   const renderListItem = (

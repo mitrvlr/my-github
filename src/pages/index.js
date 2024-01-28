5335d32d679a9f256a46f0eb0988c15538622c8b
Seongha <mitrvlr@gmail.com>
2024-01-28 12:02:59 +0900

    Update career with introduce

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ const IndexPage = () => {
     <Layout>
       <div className="layout__row">
         <div className="post__panel">
-          <h2>전성하 <i className="job">프론트엔드</i></h2>
+          <h2>전성하 <i className="job">Software Engineer</i></h2>
           <blockquote>
             <p>모든 문제에는 아직 아무도 찾아내지 못한 다른 해결책이 존재한다</p>
           </blockquote>
@@ -56,197 +56,108 @@ const IndexPage = () => {
 
           <h3>소개</h3>
           <ul>
-            <li>🎖 프론트엔드 개발합니다</li>
-            <li>🎖 코드는 문제 해결 수단 중 하나라고 생각합니다</li>
-            <li>🎖 고민의 시작부터 해결해나가는 과정을 좋아합니다</li>
-            <li>🎖 개인과 팀의 양방향으로 성장하는 문화를 지향합니다</li>
-            <li>🎖 책 읽기와 생각하기를 좋아합니다</li>
+            <li>🎖 프론트엔드 그룹의 리드를 맡고 있습니다.</li>
+            <li>🎖 그룹의 운영 방안과 비전, 목표 설정 및 성과를 관리하고 아키텍처 설계를 합니다.</li>
+            <li>🎖 그룹 구성원의 커리어 패스 설계 및 멘토링을 통한 성장을 지원합니다.</li>
+            <li>🎖 코드는 문제 해결 방법 중 하나라고 생각하고, 문제 본질에 집중합니다.</li>
+            <li>🎖 투명한 커뮤니케이션을 지향하고 타인을 존중합니다.</li>
+            <li>🎖 책을 읽고 숙고하기를 좋아합니다.</li>
           </ul>
 
           <h3>보유 기술</h3>
-          <h4>Core</h4>
-          <ul className="stack-list">
-            <li>
-              <div className="stack-list__item"><img src={html} alt="html5" /></div>
-              <strong>HTML</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={css} alt="css3" /></div>
-              <strong>CSS</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={javascript} alt="javascript" /></div>
-              <strong>JavaScript</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={typescript} alt="typescript" /></div>
-              <strong>TypeScript</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={next} alt="Next.js" /></div>
-              <strong>Next.js</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={react} alt="react" /></div>
-              <strong>react</strong>
-            </li>
+          <h4>Hard skills</h4>
+          <ul>
+            <li>-</li>
           </ul>
 
-          <h4>Environment</h4>
-          <ul className="stack-list">
-            <li>
-              <div className="stack-list__item"><img src={googleCloud} alt="googleCloud" /></div>
-              <strong>google Cloud</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={sass} alt="sass" /></div>
-              <strong>SASS</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={greensock} alt="greensock" /></div>
-              <strong>gsap</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={webpack} alt="webpack" /></div>
-              <strong>webpack</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={mysql} alt="mysql" /></div>
-              <strong>mysql</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={playwright} alt="playwright" /></div>
-              <strong>playwright</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={github} alt="github" /></div>
-              <strong>github</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={githubActions} alt="githubActions" /></div>
-              <strong>github Actions</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={eslint} alt="eslint" /></div>
-              <strong>eslint</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={stylelint} alt="stylelint" /></div>
-              <strong>stylelint</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={prettier} alt="prettier" /></div>
-              <strong>prettier</strong>
-            </li>
+          <h4>Soft skills</h4>
+          <ul>
+            <li>-</li>
           </ul>
 
-          <h4>Experience</h4>
-          <ul className="stack-list">
-            <li>
-              <div className="stack-list__item"><img src={vue} alt="vue" /></div>
-              <strong>vue</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={nuxt} alt="nuxt" /></div>
-              <strong>nuxt</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={styledComponents} alt="styledComponents" /></div>
-              <strong>StyledComponents</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={cypress} alt="cypress" /></div>
-              <strong>cypress</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={nodejs} alt="nodejs" /></div>
-              <strong>nodejs</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={express} alt="express" /></div>
-              <strong>express</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={gatsby} alt="gatsby" /></div>
-              <strong>gatsby</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={iconGraphql} alt="iconGraphql" /></div>
-              <strong>Graphql</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={docker} alt="docker" /></div>
-              <strong>docker</strong>
-            </li>
-            <li>
-              <div className="stack-list__item"><img src={netlify} alt="netlify" /></div>
-              <strong>netlify</strong>
-            </li>
+          <h4>Career Skills</h4>
+          <ul>
+            <li>Leader Ship</li>
+            <li>Follower Ship</li>
+            <li>Manager Ship</li>
           </ul>
 
           <h3>경력</h3>
           <dl>
             <dt>데이원컴퍼니 <i className="period">(2019.04.08~ 재직중)</i></dt>
+            <dd><em>2024</em> 그룹장 승진</dd>
             <dd><em>2023</em> 콜로소 서비스 내 RSC 도입 및 프론트엔드 성능 개선</dd>
-            <dd><em>2023</em> 콜로소 <a href="https://coloso.global/" target="_blank" rel="noreferrer noopener">글로벌</a> 서비스 개발, Next.js `/app`, RSC (v13.4.x) 도입</dd>
+            <dd><em>2023</em> 콜로소 <a href="https://coloso.global/" target="_blank" rel="noreferrer noopener">글로벌</a> 서비스
+              개발, Next.js `/app`, RSC (v13.4.x) 도입
+            </dd>
             <dd><em>2023</em> GCP 를 사용한 개발 환경 구성</dd>
             <dd><em>2022</em> 콜로소 국내 서비스 Next.js (v12.x) 도입</dd>
-            <dd><em>2021</em> 콜로소 <a href="https://coloso.jp/" target="_blank" rel="noreferrer noopener">일본</a>, 미국 서비스 개발 Nuxt.js (v2.x) 도입</dd>
-            <dd><em>2020</em> 콜로소 <a href="https://coloso.co.kr/" target="_blank" rel="noreferrer noopener">국내</a> Nuxt.js 도입 </dd>
-            <dd><em>2019</em> 패스트캠퍼스 웹 사이트 프론트엔드 개발, 일부 vue 전환 <a href="https://fastcampus.co.kr/" target="_blank" rel="noreferrer noopener">[링크]</a></dd>
-            <dd><em>2019</em> 프론트엔드 마크업 가이드 제작 <a href="https://github.com/fastcampusgit/markup-guide" target="_blank" rel="noreferrer noopener">[링크]</a> </dd>
+            <dd><em>2022</em> 팀장 승진</dd>
+            <dd><em>2022</em> 프로 승급</dd>
+            <dd><em>2021</em> 콜로소 <a href="https://coloso.jp/" target="_blank" rel="noreferrer noopener">일본</a>, 미국 서비스
+              개발 Nuxt.js (v2.x) 도입
+            </dd>
+            <dd><em>2021</em> 시니어 승급</dd>
+            <dd><em>2020</em> 콜로소 <a href="https://coloso.co.kr/" target="_blank"
+                                     rel="noreferrer noopener">국내</a> Nuxt.js 도입
+            </dd>
+            <dd><em>2019</em> 패스트캠퍼스 웹 사이트 프론트엔드 개발, 일부 vue 전환 <a href="https://fastcampus.co.kr/" target="_blank"
+                                                                  rel="noreferrer noopener">[링크]</a></dd>
+            <dd><em>2019</em> 프론트엔드 마크업 가이드 제작 <a href="https://github.com/fastcampusgit/markup-guide" target="_blank"
+                                                  rel="noreferrer noopener">[링크]</a></dd>
+            <dd><em>2019</em> 입사</dd>
             <dd>
-              - Next.js 인프라 레벨 어플리케이션 최적화 <br/>
-              - 프론트엔드 전체 아키텍처 설계, 컴포넌트 구조 설계와 개선 <br/>
+              - Next.js 어플리케이션과 인프라스트럭쳐 파이프라인 최적화, 개선 <br/>
+              - 프론트엔드 전체 아키텍처 설계, 개선 <br/>
               - i18n, multilingual (ui, language token) 전략 설계 <br/>
               - 데이터 모델링 참여 및 일부 설계 <br/>
-              - 소규모(5명)의 프론트엔드 팀 운영 <br />
+              - 9명 규모의 프론트엔드 그룹 운영 <br/>
             </dd>
             <dd>
               <em>사용 기술</em>
               <ul className="stack-list stack-list--organization">
                 <li>
-                  <div className="stack-list__item"><img src={html} alt="html5" /></div>
+                  <div className="stack-list__item"><img src={html} alt="html5"/></div>
                   <strong>HTML5</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={css} alt="css3" /></div>
+                  <div className="stack-list__item"><img src={css} alt="css3"/></div>
                   <strong>CSS3</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={javascript} alt="javascript" /></div>
+                  <div className="stack-list__item"><img src={javascript} alt="javascript"/></div>
                   <strong>JavaScript</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={typescript} alt="typescript" /></div>
+                  <div className="stack-list__item"><img src={typescript} alt="typescript"/></div>
                   <strong>TypeScript</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={react} alt="react" /></div>
+                  <div className="stack-list__item"><img src={react} alt="react"/></div>
                   <strong>react</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={next} alt="next" /></div>
+                  <div className="stack-list__item"><img src={next} alt="next"/></div>
                   <strong>Next.js</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={playwright} alt="playwright" /></div>
+                  <div className="stack-list__item"><img src={playwright} alt="playwright"/></div>
                   <strong>Playwright</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={styledComponents} alt="emotion" /></div>
+                  <div className="stack-list__item"><img src={styledComponents} alt="emotion"/></div>
                   <strong>Emotion.js</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={webpack} alt="webpack" /></div>
+                  <div className="stack-list__item"><img src={webpack} alt="webpack"/></div>
                   <strong>webpack</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={sass} alt="sass" /></div>
+                  <div className="stack-list__item"><img src={sass} alt="sass"/></div>
                   <strong>SASS</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={greensock} alt="greensock" /></div>
+                  <div className="stack-list__item"><img src={greensock} alt="greensock"/></div>
                   <strong>gsap</strong>
                 </li>
               </ul>
@@ -254,39 +165,39 @@ const IndexPage = () => {
               <em>환경</em>
               <ul className="stack-list stack-list--organization">
                 <li>
-                  <div className="stack-list__item"><img src={googleCloud} alt="googleCloud" /></div>
+                  <div className="stack-list__item"><img src={googleCloud} alt="googleCloud"/></div>
                   <strong>google Cloud</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={mysql} alt="mysql" /></div>
+                  <div className="stack-list__item"><img src={mysql} alt="mysql"/></div>
                   <strong>mysql</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={nodejs} alt="nodejs" /></div>
+                  <div className="stack-list__item"><img src={nodejs} alt="nodejs"/></div>
                   <strong>nodejs</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={docker} alt="docker" /></div>
+                  <div className="stack-list__item"><img src={docker} alt="docker"/></div>
                   <strong>docker</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={github} alt="github" /></div>
+                  <div className="stack-list__item"><img src={github} alt="github"/></div>
                   <strong>github</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={githubActions} alt="githubActions" /></div>
+                  <div className="stack-list__item"><img src={githubActions} alt="githubActions"/></div>
                   <strong>github Actions</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={eslint} alt="eslint" /></div>
+                  <div className="stack-list__item"><img src={eslint} alt="eslint"/></div>
                   <strong>eslint</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={stylelint} alt="stylelint" /></div>
+                  <div className="stack-list__item"><img src={stylelint} alt="stylelint"/></div>
                   <strong>stylelint</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={prettier} alt="prettier" /></div>
+                  <div className="stack-list__item"><img src={prettier} alt="prettier"/></div>
                   <strong>prettier</strong>
                 </li>
               </ul>
@@ -294,19 +205,19 @@ const IndexPage = () => {
               <em>Deprecated</em>
               <ul className="stack-list stack-list--organization">
                 <li>
-                  <div className="stack-list__item"><img src={vue} alt="vue" /></div>
+                  <div className="stack-list__item"><img src={vue} alt="vue"/></div>
                   <strong>vue</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={nuxt} alt="nuxt" /></div>
+                  <div className="stack-list__item"><img src={nuxt} alt="nuxt"/></div>
                   <strong>nuxt</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={cypress} alt="cypress" /></div>
+                  <div className="stack-list__item"><img src={cypress} alt="cypress"/></div>
                   <strong>cypress</strong>
                 </li>
                 <li>
-                  <div className="stack-list__item"><img src={express} alt="express" /></div>
+                  <div className="stack-list__item"><img src={express} alt="express"/></div>
                   <strong>express</strong>
                 </li>
               </ul>
